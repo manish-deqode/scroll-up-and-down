fe10515b09c2908070e644024e18b0aaa418e163
Manish <mthakur@deqode.com>
2021-10-07 19:16:42 +0530

    - Added the logic to adjust the scroll according to the current position of scrollbar

--- src/components/FloatButton.vue
+++ src/components/FloatButton.vue
@@ -1,15 +1,17 @@
 <template>
-    <b-button @click="moveToEnds()">Click to Scroll</b-button>
+  <b-button @click="moveToEnds()">Click to Scroll</b-button>
 </template>
 
 <script>
 export default {
   methods: {
     moveToEnds() {
-      if (window.scrollY === 0) {
-          window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+      // If at bottom of the page then scroll to top
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+         window.scrollTo(0, 0); 
       } else {
-        window.scrollTo(0, 0);
+        //scroll to bottom in other cases
+       window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
       }
     },
   },
